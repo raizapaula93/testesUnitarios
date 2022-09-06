5be5655547d5c18c015591c0a121fe81bdbc04a5
raizapaula93 <raizapaula93@gmail.com>
2022-09-05 23:55:42 -0300

    @raizapaula93[Forma robusta melhor]

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -15,10 +15,13 @@ import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
+import org.junit.rules.ExpectedException;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.exceptions.FilmeSemEstoqueException;
+import br.ce.wcaquino.exceptions.LocadoraException;
 import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoServiceTest {
@@ -26,11 +29,15 @@ public class LocacaoServiceTest {
 	
 	
 	@Rule
-	public ErrorCollector error = new ErrorCollector();//ccheca toda a a pilha de erros
+	public ErrorCollector error = new ErrorCollector();//checa toda a a pilha de erros
+	
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
 		
 	@Test //sinaliza para o framework que o método abaixo é um teste
 	
 	public void testeLocacao () throws Exception {//deixar um cursor em cima de um teste somente o executa
+		//Se o teste não estiver esperando uma exception, deixe o JUnit gerenciar
 		//cenário - inicializar as variáveis
 		LocacaoService service = new LocacaoService();
 		Usuario usuario = new Usuario("Usuário 1");
@@ -41,14 +48,14 @@ public class LocacaoServiceTest {
 		
 			locacao = service.alugarFilme(usuario, filme);
 			//verificação
-			error.checkThat(locacao.getValor(),is(equalTo(6.0)));
-			assertThat(locacao.getValor(),is(not(6.0)));
+			error.checkThat(locacao.getValor(),is(equalTo(5.0)));
 			error.checkThat(isMesmaData(locacao.getDataLocacao(),new Date()), is(true));
-			error.checkThat(isMesmaData(locacao.getDataRetorno(),DataUtils.obterDataComDiferencaDias(1)), is(false));
+			error.checkThat(isMesmaData(locacao.getDataRetorno(),DataUtils.obterDataComDiferencaDias(1)), is(true));
 
 		
 	}
-	@Test(expected=Exception.class) 
+	
+	@Test(expected=FilmeSemEstoqueException.class) //solução elegante -> Se estou esperando um exception, sinalizo para o Junit controlar
 	
 	public void testeLocacao_filmeSemEstoque () throws Exception {
 		//cenário
@@ -61,4 +68,36 @@ public class LocacaoServiceTest {
 			
 	}
 	
+	@Test
+	public void testLocacao_usuarioVazio() throws FilmeSemEstoqueException {
+		//cenario
+		LocacaoService service = new LocacaoService();
+		Filme filme = new Filme("Filme 2",1,4.0);
+		Usuario usuario = new Usuario("Usuário 1");
+		//acao
+		try {
+			service.alugarFilme(null, filme);
+				Assert.fail();
+		} catch(LocadoraException e) {
+			assertThat(e.getMessage(), is("Usuário vazio"));;
+		}
+		System.out.println("Forma robusta");
+	}
+	
+	@Test
+	
+	public void testLocacao_FilmeVazio() throws LocadoraException, FilmeSemEstoqueException {
+		//cenário
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Usuário 1");
+		
+		
+		
+		exception.expect(LocadoraException.class);
+		exception.expectMessage("Filme vazio");
+		//acao
+		service.alugarFilme(usuario, null);
+		System.out.println("Forma nova");
+	}
+		
 }
